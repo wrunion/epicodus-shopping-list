e665aaa79dd52131a067cc2db47e3546efa4da84
Winter Runion <winterrunion@gmail.com>
2019-06-18 23:42:50 -0700

    Add JS logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 $(document).ready(function() {
   $("form#shopping-list").submit(function(event) {
-    // var item1 = $("#item1").val();
-    // var item2 = $("#item2").val();
-    var items = ["item1", "item2", "item3", "item4", "item5"];
+    var item1 = $("#item1").val();
+    var item2 = $("#item2").val();
+    var item3 = $("#item3").val();
+    var item4 = $("#item4").val();
+    var item5 = $("#item5").val();
 
-    items.forEach(function(blank) {
-      var userInput = $("#" + blank).val().sort();
-      $("." + blank).text(userInput);
-    });
-    // $(".item1").text(item1);
-    // $(".item2").text(item2);
+    var items = [item1, item2, item3, item4, item5];
+
+    var sortedItems = items.sort();
+
+    alert(sortedItems);
+    // sortedItems.forEach(function(sortedItem) {
+    //   $("." + sortedItem).text(sortedItem);
+    // });
+
+    // var items = ["item1", "item2", "item3", "item4", "item5"];
+
+
+
+    // items.forEach(function(blank) {
+    //   var userInput = $("#" + blank).val();
+    //   return userInput.sort();
+    //
+    //   // $("." + blank).text(userInput);
+    // });
+
+    /*This returns the items, but doesn't alphabatize or capitalize the list*/
+    // items.forEach(function(blank) {
+    //   var userInput = $("#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
 
     $("#list-output").show();
     event.preventDefault();
